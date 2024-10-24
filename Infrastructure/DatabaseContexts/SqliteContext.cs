62d02a4f1362b0015845954fa55c04cab0529ecc
CanaanGM <Reakak1@gmail.com>
2024-10-24 19:13:49 +0300

    entity core

---
+++ Infrastructure/DatabaseContexts/SqliteContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.DatabaseContexts;
+public class SqliteContext : DbContext
+{
+    public SqliteContext(DbContextOptions<SqliteContext> options) : base(options)
+    {
+
+    }
+    public SqliteContext()
+    {
+
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseSqlite(@"E:\development\c#\AuthenticationExample\Database\Authentication_Example.sqlite");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Infrastructure.DatabaseContexts;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Infrastructure;
 
@@ -6,6 +7,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection RegisterInfrastructureServices(this IServiceCollection services)
     {
+        services.AddDbContext<SqliteContext>();
         return services;
     }
 }
